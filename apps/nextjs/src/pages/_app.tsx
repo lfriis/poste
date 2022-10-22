489a56c18abe930c094370ce4c2b5e983859328a
lfriis <larsenfriis@icloud.com>
2022-10-22 13:24:17 -0400

    patched nextjs file reference errors

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,10 +1,10 @@
 // src/pages/_app.tsx
-import "../styles/globals.css";
-import type { AppType } from "next/app";
-import { trpc } from "../utils/trpc";
+import '../styles/globals.css';
+import type { AppType } from 'next/app';
+import trpc from '../utils/trpc';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const MyApp: AppType = ({ Component, pageProps }) => (
+	<Component {...pageProps} />
+);
 
 export default trpc.withTRPC(MyApp);
--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,9 @@
 // src/pages/api/trpc/[trpc].ts
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { appRouter, createContext } from "@acme/api";
+import { createNextApiHandler } from '@trpc/server/adapters/next';
+import { appRouter, createContext } from '@acme/api';
 
 // export API handler
 export default createNextApiHandler({
-  router: appRouter,
-  createContext: createContext,
+	router: appRouter,
+	createContext,
 });
